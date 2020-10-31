4d05393fa3b58645bc2f925f09e608f7d002aa75
Eranga Heshan <ErangaHeshan@users.noreply.github.com>
2020-11-01 00:07:55 +0800

    Update TheMaximumSubarray.py (#6)

--- TheMaximumSubarray.py
+++ TheMaximumSubarray.py
@@ -1,4 +1,5 @@
 T = int(input())
+
 for i in range(T):
     input()
     array = [int(x) for x in input().split()]
@@ -7,6 +8,7 @@ for i in range(T):
     else:
         dup_array = []
         tmp = None
+        
         for j in range(len(array)):
             if j == 0:
                 tmp = array[j]
@@ -23,22 +25,22 @@ for i in range(T):
             if j == len(array) - 1:
                 dup_array.append(tmp)
 
-        # print(dup_array)
-        max_subsequent = 0
         max_subsequent1 = 0
+        max_subsequent2 = 0
         positive_sum = 0
+        
         for j in range(len(dup_array)):
-            max_subsequent = 0
+            max_subsequent1 = 0
             for k in range(j, len(dup_array)):
-                tmp = max_subsequent + dup_array[k]
-                if max_subsequent1 < tmp:
-                    max_subsequent1 = tmp
+                tmp = max_subsequent1 + dup_array[k]
+                if max_subsequent2 < tmp:
+                    max_subsequent2 = tmp
                 if tmp > 0:
-                    max_subsequent += dup_array[k]
+                    max_subsequent1 += dup_array[k]
                 else:
-                    max_subsequent = 0
+                    max_subsequent1 = 0
                     break
 
-        print(max(max_subsequent, max_subsequent1), positive_sum)
+        print(max(max_subsequent1, max_subsequent2), positive_sum)
 
 
